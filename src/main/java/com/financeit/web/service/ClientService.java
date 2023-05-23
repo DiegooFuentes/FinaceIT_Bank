52c8bbe3d16b81e73c40b110ce31fdae1f4807a2
DiegooFuentes <diegofuentesm123@gmail.com>
2023-05-23 11:42:23 -0400

    Creating Client service layer

--- src/main/java/com/financeit/web/service/ClientService.java
+++ src/main/java/com/financeit/web/service/ClientService.java
@@ -1,4 +1,17 @@
 package com.financeit.web.service;
 
+import com.financeit.web.dtos.ClientDTO;
+import org.springframework.security.core.Authentication;
+
+import java.util.List;
+
 public interface ClientService {
+
+    public List<ClientDTO> getClients();
+
+    public ClientDTO getClient(Long id);
+
+    public ClientDTO getCurrentClient(Authentication authentication);
+
+    public String register(String firstName, String lastName,String email, String password);
 }
