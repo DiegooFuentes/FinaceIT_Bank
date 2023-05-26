210a69d97d61c8399f3dbda30f23e0dacae4fa15
DiegooFuentes <diegofuentesm123@gmail.com>
2023-05-26 16:55:14 -0400

    New methods in repository

--- src/main/java/com/financeit/web/repositories/TransactionLinkRepository.java
+++ src/main/java/com/financeit/web/repositories/TransactionLinkRepository.java
@@ -8,4 +8,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 public interface TransactionLinkRepository extends JpaRepository<TransactionLink,Long> {
 
     TransactionLink findByLinkCode (String LinkCode);
+
+    boolean existsTransactionLinkByLinkCode (String LinkCode);
+
 }
